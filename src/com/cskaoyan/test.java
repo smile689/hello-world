a803ab89d70b42b977fb09bea2a0f9e38aff7a8e
smile689 <shiweixiao1990@163.com>
2018-12-03 23:22:24 +0800

    from idea hello world

---
+++ src/com/cskaoyan/test.java
@@ -0,0 +1,9 @@
+package com.cskaoyan;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("demo1: Hello World!");
+        System.out.println("demo1: Hello World!");
+        System.out.println("Hello World!");
+    }
+}
